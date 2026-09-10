8bebca09fea79320cb51d8f7ec137d4402fd7387
vrsssrikanth-glitch <vrsssrikanth@gmail.com>
2026-09-10 21:18:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,10 +77,10 @@ st.markdown(
         gap: 1.2rem;
     }
     .header-logo {
-        width: 65px;
-        height: 65px;
+        width: 75px;
+        height: 75px;
         object-fit: contain;
-        border-radius: 10px;
+        border-radius: 0px;
         background: #4F46E5 0%, #7C3AED 100%;
         padding: 5px;
         box-shadow: 0 2px 6px rgba(0,0,0,0.2);
